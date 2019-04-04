4e571736a893dbfbe13623a828393e93ddd8853c
SimonShaw25 <961197998@qq.com>
2019-04-04 23:20:30 +0800

    提交

--- hello/src/main/java/com/xzcbook/web/HelloController.java
+++ hello/src/main/java/com/xzcbook/web/HelloController.java
@@ -1,5 +1,10 @@
 package com.xzcbook.web;
 
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController
 {
 
-  @RequestMapping(value = "/hello",method = RequestMethod.GET)
+  private final Logger logger = Logger.getLogger(HelloController.class);
+
+  @Autowired
+  private DiscoveryClient client;
+
+  @RequestMapping(value = "/hello", method = RequestMethod.GET)
   public String index()
   {
+    ServiceInstance instance = client.getLocalServiceInstance();
+    logger.info("/hello, host:" + instance.getHost() + ",service_id:" + instance.getServiceId());
     return "Hello World";
   }
 }
